c94923e50cad45359220320b93385297cbf71daf
ayyuby26 <nokian5220@gmail.com>
2022-01-11 22:16:27 +0700

    init

---
+++ test/person_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_test/person.dart';
+
+void main() {
+  group("Mengecek class Person", () {
+    Person p;
+    p = Person();
+
+    test("Inisialisasi Objek Person", () {
+      print("test 1");
+      expect(p.name, equals("no name"));
+      expect(p.age, equals(0));
+    });
+
+    test("Age harus positif", () {
+      print("test 2");
+      p.age = -5;
+      expect(p.age, isPositive);
+    });
+
+    test("Angka keberuntungan harus 3 buah bilangan positif", () {
+      print("test 3");
+      expect(p.luckyNumbers,
+          allOf([hasLength(equals(3)), isNot(anyElement(isNegative))]));
+    });
+
+    setUp(() {
+      print("set up");
+    });
+
+    tearDown(() {
+      print("tear down");
+    });
+  });
+}
